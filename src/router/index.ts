1c10cffffa3f61d4c6b379b1c5b30beeae204d12
Dionésio Guerra <dionesiojr@gmail.com>
2025-08-31 14:23:54 -0300

    fix: update SPA routing and favicon
    - Update fallback route to redirect to /home
    - Replace favicon with improved version

--- src/router/index.ts
+++ src/router/index.ts
@@ -36,6 +36,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterPage
+    },
+    {
+      // Rota de fallback para capturar todas as rotas não encontradas
+      path: '/:pathMatch(.*)*',
+      redirect: '/home'
     }
   ]
 })
